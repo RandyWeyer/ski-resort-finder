ebefe55cf15fa2b49d407081d7649ab237ecb342
Guest User <Guest@epicodus-19.local>
2018-04-09 13:58:29 -0700

    get user location and show it on the embedded map

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,60 @@
-// map = new google.maps.Map(document.getElementById('map'), {
-//   center: {lat: -34.397, lng: 150.644},
-//   zoom: 8
-// });
+var map;
+var userLocation = {"lat":45.084722,"lng":-69.905556};
+
 function myMap() {
   var mapProp= {
-    center:new google.maps.LatLng(45.084722,-69.905556),
+    center:new google.maps.LatLng(userLocation.lat,userLocation.lng),
     zoom:5,
   };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var marker = new google.maps.Marker({
+    position: userLocation,
+    map: map,
+    title: "Your Location"
+  });
 }
+
+function geoFindMe() {
+  var output = document.getElementById("output");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    userLocation.lat = position.coords.latitude;
+    userLocation.lng = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + userLocation.lat + ' <br>Longitude is ' + userLocation.lng + '</p>';
+    console.log(userLocation);
+  }
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function newLocation(newLat,newLng)
+{
+	map.setCenter({"lat":newLat, "lng":newLng});
+}
+
 
 $(document).ready(function(){
 
+  $("#share-location").click(function(event) {
+    event.preventDefault();
 
+    geoFindMe();
 
-
-
+  });
+  $("#change-location").click(function(event) {
+    event.preventDefault();
+    newLocation(userLocation.lat,userLocation.lng);
+  });
 });
-
-
-// $("#map-coordinates").submit(function(event) {
-//   event.preventDefault();
-//
-//
-// });
