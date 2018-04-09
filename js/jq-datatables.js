b61d11d89df94ecd53aae25f2f171c1350a1725b
David Tumpowsky and Justin Roller <david@>
2018-04-09 11:54:49 -0700

    incorporate function toparse json file into javascript

---
+++ js/jq-datatables.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  var skiTable = $('#resort-specs').DataTable({
+
+  });
+
+  var tableInfo = JSON.parse('{"name": "Baker Mountain"}');
+  console.log(tableInfo.name)
+
+
+
+});
