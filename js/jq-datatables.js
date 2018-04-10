35d9992f97871036b679198c1764355158d55650
Randy Weyer <RandyPWeyer@gmail.com>
2018-04-10 08:04:13 -0700

    DataTable populates before creating the table

--- js/jq-datatables.js
+++ js/jq-datatables.js
@@ -3,24 +3,16 @@
 
 $(document).ready(function() {
 
+  skiData.forEach(function(instance) {
+    $("#result").append(
+    "<tr><td>" + instance.name + "</td><td>" + instance.nearestTown + "</td><td>" + instance.state + "</td><td>" + instance.baseElevation + "</td><td>" + instance.verticalFeet + "</td><td>" + instance.runs + "</td><td>" + instance.website + "</td></tr>")
+  });
+  
   var skiTable = $('#resort-specs').DataTable({
 
 
   });
 
-
-  // var lengthOfSki = Object.keys(skiData).length
-  // for(var i = 0; i < lengthOfSki; i ++){
-    skiData.forEach(function(instance) {
-      $("#result").append(
-      "<tr><td>" + instance.name + "</td><td>" + instance.nearestTown + "</td><td>" + instance.state + "</td><td>" + instance.baseElevation + "</td><td>" + instance.verticalFeet + "</td><td>" + instance.runs + "</td><td>" + instance.website + "</td></tr>")
-    });
-
-
-
  console.log(skiData)
-
-
-
 
 });
