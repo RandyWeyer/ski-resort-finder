0246dfa6a7e3b01b65b1f7a1b6998419565ad0b6
Justin Roller and Randy Weyer <justin@>
2018-04-10 08:54:17 -0700

    Change datatype of numbers from string to number

--- js/jq-datatables.js
+++ js/jq-datatables.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 
   skiData.forEach(function(instance) {
     $("#result").append(
-    "<tr><td>" + instance.name + "</td><td>" + instance.nearestTown + "</td><td>" + instance.state + "</td><td>" + instance.baseElevation + "</td><td>" + instance.verticalFeet + "</td><td>" + instance.runs + "</td><td>" + instance.website + "</td></tr>")
+    "<tr><td>" + instance.name + "</td><td>" + instance.nearestTown + "</td><td>" + instance.state + "</td><td>" + (instance.baseElevation*1) + "</td><td>" + (instance.verticalFeet*1) + "</td><td>" + (instance.runs*1) + "</td><td>" + instance.website + "</td></tr>")
   });
-  
+
   var skiTable = $('#resort-specs').DataTable({
 
 
